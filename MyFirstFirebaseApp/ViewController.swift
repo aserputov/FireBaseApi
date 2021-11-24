cec5a6b9366e4dc6220ec674cc4d2681866eed4d
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-24 03:27:11 -0500

    first commit

---
+++ MyFirstFirebaseApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFirstFirebaseApp
+//
+//  Created by zebra on 2021-05-13.
+//  Copyright © 2021 zebra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func saveAll(_ sender: Any) {
+    }
+    @IBAction func getAll(_ sender: Any) {
+    }
+}
+
